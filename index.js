9f1e618c30f22a3cddbc06b71f4788024f5e9b50
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2022-12-20 19:18:16 +0600

    Added post and delete Api

--- index.js
+++ index.js
@@ -34,6 +34,18 @@ const run = async () => {
       res.send({ status: true, data: product });
     });
 
+    app.post("/product" , async (req,res) =>{
+      const product =  req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    })
+
+    app.delete("/product/:id" , async (req , res) =>{
+      const id = req.params.id;
+      const result = await productCollection.deleteOne({_id : ObjectId(id)})
+      res.send(result);
+    })
+
   } finally {
   }
 };
@@ -41,8 +53,9 @@ const run = async () => {
 run().catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Pc-Hub!");
 });
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
